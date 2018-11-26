bc78465d8e924140fb3a4468de0cec739fad6450
shugan <shugan@2dfire.com>
2018-11-26 11:18:45 +0800

    first commit

---
+++ src/main/java/com/example/deeprocketmq/DeepRocketmqApplication.java
@@ -0,0 +1,12 @@
+package com.example.deeprocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepRocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepRocketmqApplication.class, args);
+    }
+}
